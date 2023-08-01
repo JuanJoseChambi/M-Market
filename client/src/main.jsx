a6a1908e83246da548c01bc463aac6e0ae3f0110
Juan Jose <chambijuanjose05@gmail.com>
2023-08-01 15:57:25 -0300

    Config/viewWeb

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider} from 'react-redux';
 import { store } from './redux/store.js'
 import {BrowserRouter} from 'react-router-dom';
+import "./main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store} >
