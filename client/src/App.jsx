d2e08e0b6f46305cc9f5ebbdc287cbb730ece9b8
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-08-02 16:12:19 -0300

    agrega axios y ruta en App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./view/Home/Home";
 import Form from "./view/Form/Form";
+import axios from "axios";
+axios.defaults.baseURL="http://localhost:3001";
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route exact path="/" element={<Home/>} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/form" element={<Form/>}/>
       </Routes>
