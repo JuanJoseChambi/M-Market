9de3f9c5805778084051dd72e24c1d23eb8e132d
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-08-02 15:30:27 -0300

    update basico

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import reducer from './reducer';
-import thunkMiddleware from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit'
+import reducerSlice from './reducer'
 
 
-const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // esta linea es para conectar don la extensnion del navegador => REDUX D
+export const store = configureStore({
+  reducer: {
+    products: reducerSlice
+  }
 
-export const store = createStore(
-    reducer,
-    composeEnhacer(applyMiddleware(thunkMiddleware))); 
+})
 
-    export default store;  
+export default store;
