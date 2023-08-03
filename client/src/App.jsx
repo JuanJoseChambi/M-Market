30407aa6aeb2e925bb611a9eb9b92046788f7613
Juan Jose <chambijuanjose05@gmail.com>
2023-08-02 23:43:28 -0300

    Feature/Detail & Feature/Form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./view/Home/Home";
 import Form from "./view/Form/Form";
+import Detail from "./view/Detail/Detail";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/form" element={<Form/>}/>
+        <Route exact path="/detail/:id" element={<Detail/>}/>
       </Routes>
     </div>
   );
--- client/src/view/Detail/Detail.jsx
+++ client/src/view/Detail/Detail.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./Detail.module.css"
+import { NavLink, useParams } from "react-router-dom";
 
-function Detail() {
+export default function Detail() {
+    const { id } = useParams()
+    const [button, setButton] = useState(false)
+
+    function handlerButton(){
+        setButton(!button)
+    }
+
   return (
-    <div>
+    <div className={style.detailView}>
       <NavLink to="/home">
         <button className={style.returnBack}>Home</button>
       </NavLink>
+      <div className={style.cardProduct}>
+        <div className={style.containerImg}>
+            <img src="https://jumboargentina.vtexassets.com/arquivos/ids/427751-800-auto?v=636495154762100000&width=800&height=auto&aspect=true%22," alt="" className={style.image} />
+        </div>
+        <div className={style.containerInfoProduct}>
+            <h2 className={style.nameProduct}>Galletitas Dulces Rellenas Con Crema Oreo 354g</h2>
+            <div className={style.brandUnitContainer}>
+                <h5 className={style.brandProduct}>Oreo</h5>
+                <h5 className={style.unitProduct}>| Unidad: 150</h5>
+            </div>
+            <b className={style.scoreProduct}>⭐⭐⭐⭐⭐</b>
+            <p className={style.descountProduct}>$<s>1.120,89</s><p className={style.descount}>30%</p></p>
+            <h3 className={style.priceProduct}>$959,92</h3>
+            <p className={style.priceRegularProduct}>Precio Regular: $2.711,64 x kg</p>
+            {button
+            ?<button className={style.btn}>Comprar<button className={style.changerButton} onClick={handlerButton}><i class='bx bx-rotate-right'></i></button></button>
+            :<button className={style.btn}>Agregar<button className={style.changerButton} onClick={handlerButton}><i class='bx bx-rotate-right'></i></button></button>}
+        </div>
+      </div>
+      <div className={style.otherInfo}>
+        <div className={style.blockInfo}>
+            <div className={style.containersTabs}>
+                <button className={style.tabs}>Description</button>
+                <button className={style.tabs}>Comentarios</button>
+            </div>
+            <div className={style.blockView}>
+                Description
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default Detail;
