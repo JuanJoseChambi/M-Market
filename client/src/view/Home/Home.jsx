30eeddc02b0bde57264777573a64f6939897c7c2
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-08-04 07:36:40 -0300

    Actualizacion Home & Paginado

--- client/src/view/Home/Home.jsx
+++ client/src/view/Home/Home.jsx
@@ -1,51 +1,44 @@
-import ProductContainer from "../../components/ProductContainer/ProductContainer";
-import Nav from '../../components/Nav/Nav';
-import {  useDispatch,  } from "react-redux"
-import { useEffect } from "react";
-import axios from "axios";
-import { setProducts } from "../../redux/reducer";
-
-
+import React from 'react';
+import Nav from "../../components/Nav/Nav";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import {allProducts} from "../../redux/slices/productsData";
+import Paginado from '../../components/Paginado/Paginado';
 
 export default function Home() {
-
   const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products)
 
-  async function allProducts () {
-    const { data } = await axios.get("/product");
-    console.log(data)
-  if (data) {
-    const info = await data.map((e) => {
-      
-      return {
-        id: e.id,
-        name: e.name,
-        image: e.image,
-        price: e.price,
-      };
-    });
-    dispatch(setProducts(info))
-  }
-  }
+   //PAGINATION VARS
+   let [currentPage, setCurrentPage] = useState(1)
+   let cardsInPage = 20
+ 
+   const lastIndex = currentPage * cardsInPage
+   const firstIndex = lastIndex - cardsInPage
+   const cardsShowed = (function () {
+     if (products.length === 0) return products //Si no hay productos para mostrar
+     else return products?.slice(firstIndex, lastIndex) //Dividimos el array original con los proveedores a mostrar
+   })()
+ 
+   const setPagina = (num) => {
+     setCurrentPage(num)
+   }
+ 
+   useEffect(() => {
+     dispatch(allProducts())
+     
+   }, [dispatch])
 
-  useEffect(() => {
-    async function info () {
-      await allProducts()
-    }
-    info()
-   }, []);
 
   return (
     <div>
       <div>
         <Nav />
       </div>
-         <h1>Mini Market</h1>
-         <ProductContainer />
+      <h1>Mini Market</h1>
       
-     </div>
+      <Paginado currentPage={currentPage} cardsInPage={cardsInPage} totalCards={products?.length} setPagina={setPagina} />
+    
+    </div>
   );
-
-  }
-
- 
+}
