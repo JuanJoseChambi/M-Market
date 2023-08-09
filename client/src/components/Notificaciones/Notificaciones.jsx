50f3b2bd9476f8b66781b61183534af2c3c44f3a
Juan Jose <chambijuanjose05@gmail.com>
2023-08-08 23:41:54 -0300

    TestSocket

--- client/src/components/Notificaciones/Notificaciones.jsx
+++ client/src/components/Notificaciones/Notificaciones.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react"
+import io from "socket.io-client"
 
 function Notificaciones() {
+
+    const socket = io("/")
+
+    useEffect(() => {   
+        socket.emit("notificaciones", data)
+    }, [])
+
   return (
     <div>Notificaciones</div>
   )
