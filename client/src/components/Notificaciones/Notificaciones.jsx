1287fcdf0d43b8f9c0f55c8381b54433a6d7c611
Juan Jose <chambijuanjose05@gmail.com>
2023-08-08 23:44:05 -0300

    TestSocket

--- client/src/components/Notificaciones/Notificaciones.jsx
+++ client/src/components/Notificaciones/Notificaciones.jsx
@@ -4,7 +4,8 @@ import io from "socket.io-client"
 function Notificaciones() {
 
     const socket = io("/")
-
+    // socket.emit("feedbacks", feedBack) para enviar al server de socket.io, con el nombre de referencia y el dato que se envia,
+    // el cual era un estado en otro codigo. 
     useEffect(() => {   
         socket.emit("notificaciones", data)
     }, [])
