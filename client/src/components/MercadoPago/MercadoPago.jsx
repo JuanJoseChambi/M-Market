87bbce9659221a7fd79f3881c53ba162163c9add
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-08-10 16:00:11 -0300

    Modifico Cart y MercadoPago

--- client/src/components/MercadoPago/MercadoPago.jsx
+++ client/src/components/MercadoPago/MercadoPago.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import axios from "axios";
 import styles from "./MercadoPago.module.css";
+import Swal from "sweetalert2";
 
 const MercadoPago = () => {
   const [preferenceId, setPreferenceId] = useState(null);
   const [fadeButton, setFadeButton] = useState(false); // Estado para controlar el desvanecimiento
-
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
+  
   useEffect(() => {
     initMercadoPago("TEST-ccf47120-6097-4b48-9ac0-ba94be0334fe");
   }, []);
@@ -26,24 +29,46 @@ const MercadoPago = () => {
   };
 
   const handleBuy = async () => {
-    const id = await createPreference();
-    if (id) {
-      setPreferenceId(id);
-      setFadeButton(true); // Activar el desvanecimiento al hacer clic en el botón
+    const shouldProceed = await Swal.fire({
+      title: "¿Deseas continuar con el proceso de pago?",
+      icon: "question",
+      background: "aliceblue",
+      showCancelButton: true,
+      confirmButtonText: "Sí",
+      cancelButtonText: "Cancelar",
+      customClass: {
+        confirmButton: styles.customConfirmButton, 
+        title: styles.customTitleFont,
+      },
+    });
+  
+    if (shouldProceed.isConfirmed) {
+      const id = await createPreference();
+      if (id) {
+        setPreferenceId(id);
+        setShowConfirmation(false); // Ocultar la alerta de confirmación
+        setPaymentCompleted(true); // Mostrar el botón de MercadoPago y ocultar el botón "Pagar"
+      }
     }
   };
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Mercado Pago</h2>
-      <button
-        className={`${styles.button} ${fadeButton ? styles["fade-out"] : ""}`} // Aplicar la clase de desvanecimiento si se activa
-        onClick={handleBuy}
-      >
-        Pagar
-      </button>
+  <h2 className={styles.title}>Mercado Pago</h2>
+  {paymentCompleted ? (
+    <>
       {preferenceId && <Wallet initialization={{ preferenceId }} />}
-    </div>
+    </>
+  ) : (
+    <button
+      className={`${styles.button} ${fadeButton ? styles["fade-out"] : ""}`}
+      onClick={handleBuy}
+    >
+      Pagar
+    </button>
+  )}
+</div>
+
   );
 };
 
