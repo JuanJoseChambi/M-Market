85651ea619945bca65207ce1dc1075fd25d42297
apardov <and.pardo.vergara@gmail.com>
2023-08-10 19:25:22 -0400

    login con boton de pago

---
+++ client/src/redux/slices/userAuth.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isAuthenticated: false,
+  },
+  reducers: {
+    loginSuccess: (state) => {
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+
+export default authSlice.reducer;
