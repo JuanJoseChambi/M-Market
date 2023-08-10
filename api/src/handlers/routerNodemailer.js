5ae8beff889dd1872a181c5f9bb723dba0f0667c
Juan Jose <chambijuanjose05@gmail.com>
2023-08-10 15:29:01 -0300

    Nodemailer

--- api/src/handlers/routerNodemailer.js
+++ api/src/handlers/routerNodemailer.js
@@ -145,11 +145,10 @@ const sendMail = async (req, res) => {
 };
 
 const register = async (req, res) => {
-  // const { name } = req.body;
+  const {email} = req.body;
   try {
     // Crear un token de acceso válido utilizando el cliente OAuth 2.0
     const accessToken = await oAuth2Client.getAccessToken();
-
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
@@ -164,32 +163,54 @@ const register = async (req, res) => {
     });
     const mailOptions = {
       from: "MMarket <chambijuanjose05@gmail.com>",
-      to: "chambijuanjose05@gmail.com",
+      to: email,
       subject: "M-Market | ¡Bienvenido a M-Market! 🎉",
       html: `
       <html>
       <head>
         <style>
-        .TitleNotification {
-          text-align:center;
-          width: 95%;
-          height: auto;
-          background-color: #ff8000;
-          border-radius: 10px 10px 0 0;
+        .MMarketBlock{
+          display:flex;
+          width: 90%;
+          min-height: 50vh;
           border: 1px solid #0000003a;
-          padding: 20px;
-          color: black;
-          margin: 0;
+          background-color: #ffffff;
+          padding:20px;
+          border-radius: 20px;
         }
-        .BodyNotification{
+        .LogoMMarket{
+          display: grid;
+          place-items: center;
+          width: 30%;
+          min-height: 50vh;
+          border: 1px solid #0000003a;
+          background-color: #f5a418;
+          border-radius: 20px 0px 0px 20px;
+          position:relative;
+        }
+        .Imagen{
+          width:100%;
+        }
+        .TitleNotification {
+          width: 100%;
+          height: auto;
+          text-align:center;
+          background-color: transparent;
+          color: black;
+          font-size:19px;
+          margin: 0;
+          color:white;
+        }
+        .TextNotification{
           display:inline-block;
           text-align:center;
-          width: 95%;
+          width: 70%;
           min-height: 50vh;
-          border-radius: 0px 0px 20px 20px;
+          border-radius: 0px 20px 20px 0px;
           border: 1px solid #0000003a;
           padding: 20px;
           margin: 0;
+          font-size:12px;
         }
         .message {
           font-style: italic;
@@ -199,17 +220,22 @@ const register = async (req, res) => {
         </style>
       </head>
       <body>
-        <h1 class="TitleNotification">¡Bienvenido a M-Market! 🎉</h1>
-        <div class="BodyNotification">
-          <h2 class="message">Hola Jose,</h2>
-          <h3 class="message">¡Bienvenido a M-Market! Nos alegra verte aquí y queremos darte una cálida bienvenida a nuestra comunidad. Estamos emocionados de que hayas decidido unirte a nosotros para descubrir productos increíbles y una experiencia de compra única.<br>
-          En M-Market, no solo encontrarás una amplia gama de productos de alta calidad, sino que también tendrás acceso a ofertas especiales, promociones exclusivas y actualizaciones sobre las últimas tendencias. Queremos que te sientas como en casa mientras exploras nuestro catálogo y haces tus compras.<br>
-          ¡No dudes en navegar por nuestras categorías y explorar lo que tenemos para ofrecerte! Si tienes alguna pregunta, comentario o necesitas ayuda en cualquier momento, nuestro equipo de atención al cliente está aquí para ayudarte.<br>
-          Como agradecimiento por unirte a nosotros, te ofrecemos un descuento de 15% especial en tu próxima compra. Solo tienes que aplicar el código "[Código de Descuento]" al finalizar tu compra para aprovecharlo.<br>
-          Una vez más, te damos la bienvenida y esperamos que disfrutes de una experiencia de compra excepcional en M-Market. ¡Esperamos verte pronto explorando nuestros productos y encontrando lo que más te guste!</h3>
-          <p class="message">¡Saludos y felices compras!<br>
-          Atentamente, <br>
-          El Equipo de M-Market</p>
+        <div class="MMarketBlock">
+          <div class="LogoMMarket">
+              <img class="Imagen" src="https://res.cloudinary.com/dvu3hvpzu/image/upload/v1691689314/p5mzh6ueddaacipen4s9.jpg" alt="Imagen">
+              <h1 class="TitleNotification">¡Bienvenido a M-Market! 🎉</h1>
+          </div>
+          <div class="TextNotification">
+            <h2 class="message">!Hola</h2>
+            <h3 class="message">¡Bienvenido a M-Market! Nos alegra verte aquí y queremos darte una cálida bienvenida a nuestra comunidad. Estamos emocionados de que hayas decidido unirte a nosotros para descubrir productos increíbles y una experiencia de compra única.<br>
+            En M-Market, no solo encontrarás una amplia gama de productos de alta calidad, sino que también tendrás acceso a ofertas especiales, promociones exclusivas y actualizaciones sobre las últimas tendencias. Queremos que te sientas como en casa mientras exploras nuestro catálogo y haces tus compras.<br>
+            ¡No dudes en navegar por nuestras categorías y explorar lo que tenemos para ofrecerte! Si tienes alguna pregunta, comentario o necesitas ayuda en cualquier momento, nuestro equipo de atención al cliente está aquí para ayudarte.<br>
+            Como agradecimiento por unirte a nosotros, te ofrecemos un descuento de 15% especial en tu próxima compra. Solo tienes que aplicar el código "[Código de Descuento]" al finalizar tu compra para aprovecharlo.<br>
+            Una vez más, te damos la bienvenida y esperamos que disfrutes de una experiencia de compra excepcional en M-Market. ¡Esperamos verte pronto explorando nuestros productos y encontrando lo que más te guste!</h3>
+            <p class="message">¡Saludos y felices compras!<br>
+            Atentamente, <br>
+            El Equipo de M-Market</p>
+          </div>
         </div>
       </body>
     </html>
