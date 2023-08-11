85740eddbaed8016e3ad359697e655cb2458436a
Juan Jose <chambijuanjose05@gmail.com>
2023-08-10 22:05:29 -0300

    Numero en el Carrito

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,17 +3,27 @@ import Sidebar from './Sidebar.jsx';
 import Search from '../Search/Search';
 import LogOut from '../../view/Login/Logout.jsx';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import CartSlide from '../Cart/CartSlide';
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
+  const { cart } = useSelector(state => state.products) 
 
+  const prod = cart.length;
+  const [productsInCart, setProductsInCart] = useState(prod)
   const[openedCart, setOpenedCart] = useState(false);
+
+
   const showCart = () => {
     setOpenedCart(!openedCart);
   };
-
+  useEffect(() => {
+    setProductsInCart(prod)
+  }, [prod])
+  
+  // console.log(productsInCart);
   return (
     <div className='nav_container'>
       <div className="sideBar">
@@ -32,7 +42,7 @@ const Nav = () => {
             <button className="icons"><i className='bx bx-heart'></i></button>
           </Link>
           <Link to="#">
-            <button className="icons" onClick={showCart}><i className='bx bx-cart'></i></button>
+            <button className="icons" onClick={showCart}><i className='bx bx-cart'></i>{productsInCart === 0? null :<div className="NumberInCart">{productsInCart}</div>}</button>
           </Link>
           <div className={openedCart ? 'cart_menu active' : 'cart_menu'}>
             <Link to='#' onClick={showCart} className='cart_menu_close'>
