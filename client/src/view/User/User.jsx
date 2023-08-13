a9273c0965c95cd352b022e8baeb3cae500b53be
Juan Jose <chambijuanjose05@gmail.com>
2023-08-12 21:35:52 -0300

    Modularizado User

--- client/src/view/User/User.jsx
+++ client/src/view/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import style from "./User.module.css";
 import { NavLink } from "react-router-dom";
 import Updateusers from "./RenderView/Updateusers/Updateusers"
