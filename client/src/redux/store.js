661bfa5d2fc0823e32f14e7c0e139f92e7b75143
Juan Jose <chambijuanjose05@gmail.com>
2023-08-15 00:16:31 -0300

    DashBoardAdmin

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsData from './slices/productsData'
 import userAuth from './slices/userAuth';
+import searchUser from "./slices/searchUser"
 
 
 export const store = configureStore({
@@ -8,6 +9,7 @@ export const store = configureStore({
     //AQUI VAN LOS REDUCERS DE CADA SLICE
     products: productsData,
     auth: userAuth,
+    search: searchUser
   }
 
 })
