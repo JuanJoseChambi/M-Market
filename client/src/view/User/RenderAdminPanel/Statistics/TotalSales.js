94a5556a012f39059c59d6bd3a600c266bb10ecb
apardov <and.pardo.vergara@gmail.com>
2023-08-16 02:28:36 -0400

    graficos statistics

---
+++ client/src/view/User/RenderAdminPanel/Statistics/TotalSales.js
@@ -0,0 +1,39 @@
+const TotalSales = [
+    {
+        month: 'Enero',
+        sales: 10000000
+    },
+    {   
+        month: 'Febrero',
+        sales: 30000000,
+    },
+    {
+        month: 'Marzo',
+        sales: 25000000,
+    },
+    {
+        month: 'Abril',
+        sales: 27800000,
+    },
+    {
+        month: 'Mayo',
+        sales: 18900000,
+    },
+    {
+        month: 'Junio',
+        sales: 23900000,
+    },
+    {
+        month: 'Julio',
+        sales: 34900000,
+    },
+    {
+        month: 'Agosto',
+        sales: 40000000,
+    },
+];
+
+export default TotalSales;
+
+        
+
