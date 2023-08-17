28d8200f08b4ead60209e215a90516cfe1a88700
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-08-17 11:48:58 -0300

    ok

--- api/src/handlers/deliveryHandler.js
+++ api/src/handlers/deliveryHandler.js
@@ -2,10 +2,10 @@ const {newDelivery, getAllDelivery} = require('../controllers/dliveryCtrl')
 
 const postDelivery = async (req, res) => {
     try {
-        const { receives, address, phone, pickUp, delivery, userID } = req.body;
-        if (![receives, address, phone, pickUp, delivery, userID].every(Boolean)) 
+        const { receives, address, phone, pickUp, delivery, userEmail } = req.body;
+        if (![receives, address, phone, pickUp, delivery, userEmail].every(Boolean)) 
         return res.status(404).send('Flata enviardatos obligatorios')
-        const aux = await newDelivery(receives, address, phone, pickUp, delivery, userID)
+        const aux = await newDelivery(receives, address, phone, pickUp, delivery, userEmail)
         res.status(201).json(aux)
     } catch (error) {
         res.status(400).json({ error: error.message })
