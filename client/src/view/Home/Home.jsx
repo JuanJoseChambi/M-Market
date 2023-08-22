a1a647cecf74ffa11f7cd47a9eec1e74756bad2c
Jonathan Fernandez <arcancode@gmail.com>
2023-08-22 16:25:04 -0300

    retoques10

--- client/src/view/Home/Home.jsx
+++ client/src/view/Home/Home.jsx
@@ -66,7 +66,8 @@ export default function Home() {
 
         <div className="container">
           <div className="row justify-content-center">
-            {cardsShowed.length !== 0
+            
+            {/* {cardsShowed.length !== 0
             ? cardsShowed.map((item) => (
               item.unit !== 0 
               ?(item.state 
@@ -85,7 +86,7 @@ export default function Home() {
                 <img className={styles.imageEmptyUser} src={productEmpty} alt="El producto no existe" />
                 <i className={styles.noUserText}>El producto no existe</i>
               </div> 
-            }
+            } */}
           </div>
         </div>
         
